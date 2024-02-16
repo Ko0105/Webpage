9c6b9b40af3d19e8c624a0bf5b6aa8ccd9afe1e9
ko@KoChiJyunde-MBP <ws612531@gmail.com>
2024-02-16 16:03:02 +0800

    upload webpage ini

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import MyProfile from '@/view/MyProfile'
+import './index.css'
+function App() {
+
+  return (
+    // <div>
+    //   <Button>Click me</Button>
+    // </div>
+    <>
+    <MyProfile />
+    </>
+    
+  )
+  
+}
+
+export default App
