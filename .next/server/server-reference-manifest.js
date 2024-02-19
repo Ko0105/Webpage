6f16d97e5d2cb2eae22891a0e7735e6e15048b06
ko@KoChiJyunde-MBP <ws612531@gmail.com>
2024-02-19 15:20:09 +0800

    add json location

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"o5n4Sr64dcUXonjHxi8R9uhUMvg/y0AmR1efyH7T2G8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xkQoIbxXi6Rk+hAaPy3EbZT5J6I9uoG/S5RoTQZbpE8=\"\n}"
